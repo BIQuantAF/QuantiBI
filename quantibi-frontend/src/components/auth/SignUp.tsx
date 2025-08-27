9320f86223bdb1ba873e626fee2310a2e664595d
BIQuantAF <atfitzgerald13@gmail.com>
2025-08-27 17:44:19 +0100

    sign up text

--- quantibi-frontend/src/components/auth/SignUp.tsx
+++ quantibi-frontend/src/components/auth/SignUp.tsx
@@ -97,6 +97,9 @@ const SignUp: React.FC = () => {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            This tool is currently in beta, not all features may be available.
+          </h2>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Create your account
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
